825e4715e10159cf5c971fac53f9b590a913ccf7
Alberto <alberto.aeraph@gmail.com>
2019-07-13 23:21:42 -0300

    Start to proxi routes to api equipment

--- app/routes/app.core.routes.ts
+++ app/routes/app.core.routes.ts
@@ -1,27 +1,14 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { UserRouters } from './core/user.routes';
-import { CompanyRouters, CompanyAddressRouters } from './core/company.routes';
 import { LoginRoutes } from './core/login.routes';
-//import { middleware } from '../../config/index.middleware';
-import { ScheduleRoutes } from './core/schedule.routes';
-import { RecordRoutes } from './core/record.routes';
-import * as passport from 'passport';
-import { UserController } from '../modules/core/controllers/user.controller';
 
-const userController: UserController = new UserController()
+import * as passport from 'passport';
+
+import EquipamentApiRoutes from '../routes/equipment-api/equipament-api.routes';
+
+
 
 let router = Router();
-
-// // middleware that is specific to this router
-// middleware.forEach(iten => {
-//     router.use(iten.action);
-// });
-
-// Wellcome Route
-router.get('/', function (req: Request, res: Response) {
-    userController.create
- 
-});
 
 
 
@@ -32,29 +19,13 @@ router.use(
     '/core',
     router.use(
         '/users',
-       
         UserRouters
-    ),
-    router.use(
-        '/companies',
-        passport.authenticate('jwt', { session: false }),
-        CompanyRouters
-    ),
-    router.use(
-        '/companies/address',
-        passport.authenticate('jwt', { session: false }),
-        CompanyAddressRouters
-    ),
-    router.use(
-        '/schedules',
-        passport.authenticate('jwt', { session: false }),
-        ScheduleRoutes
-    ),
-    router.use(
-        '/records',
-        passport.authenticate('jwt', { session: false }),
-        RecordRoutes
     )
+);
+
+router.use(
+    '/equipamento',
+    EquipamentApiRoutes
 );
 
 export default router;
---
+++ app/routes/equipment-api/equipament-api.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import * as passport from 'passport';
+import * as httpProxy from 'express-http-proxy';
+import RoutePathService from '../../shared/services/route-path.service';
+
+const serviceProxy = httpProxy('api-equipments:5001', {
+    https: true,
+    proxyReqPathResolver: function (req) {
+        const url = RoutePathService.getRoute(req).split('/equipamento');
+        const route = '/api/v1/' + url[1]
+        return route;
+    },
+    proxyReqOptDecorator: function (proxyReqOpts, originalReq) {
+        proxyReqOpts.rejectUnauthorized = false;
+        return proxyReqOpts;
+    }
+});
+
+let routers = Router();
+
+// Define the Equipment Api Proxi Routes (Protected)
+routers.use(
+    '/',
+    (req, res, next) => {
+        serviceProxy(req, res, next);
+    }
+);
+
+export default routers;
