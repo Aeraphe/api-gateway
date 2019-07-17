89e7f6b91f91d6a22fb7c3c467720c01e3c092f7
root <alberto.aeraph@gmail.com>
2019-07-17 09:56:08 +0000

    Build

--- dist/routes/app.core.routes.js
+++ dist/routes/app.core.routes.js
@@ -4,10 +4,12 @@ var express_1 = require("express");
 var user_routes_1 = require("./core/user.routes");
 var login_routes_1 = require("./core/login.routes");
 var equipament_api_routes_1 = require("../routes/equipment-api/equipament-api.routes");
+var inventory_api_routes_1 = require("../routes/inventory-api/inventory-api.routes");
 var router = express_1.Router();
 router.use('/login', login_routes_1.LoginRoutes);
 // Define the Core routes (Protected)
 router.use('/core', router.use('/users', user_routes_1.UserRouters));
 router.use('/equipamento', equipament_api_routes_1.default);
+router.use('/estoque', inventory_api_routes_1.default);
 exports.default = router;
 //# sourceMappingURL=app.core.routes.js.map
