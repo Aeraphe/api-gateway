070854e8bdc0be96fa004f1d105d6291af0f5b28
Alberto <alberto.aeraph@gmail.com>
2019-07-21 23:56:12 +0000

    Delete old code

--- app/routes/core/record.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import RecordController from '../../modules/core/controllers/record.controller';
-
-export const RecordRoutes = Router();
-
-RecordRoutes.post('/', RecordController.create);
--- app/routes/core/schedule.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import ScheduleController from '../../modules/core/controllers/schedule.controller'
-export const ScheduleRoutes = Router();
-
-
-
-ScheduleRoutes.post("/",ScheduleController.create)
